56ccda82543e80ddbc89c1f2f9b93b8df8d30b89
Da-Wei Lee <daviddwlee84@gmail.com>
2024-05-10 11:05:54 +0800

    Able to get feed sources list

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from fastapi_scheduler import SchedulerAdmin
 from contextlib import asynccontextmanager
 from RSubscribeSummarizer.data.fetcher import RSSFeedFetcher
 from RSubscribeSummarizer.data.parser import RSSHubFeedParser
+from RSubscribeSummarizer.data.model import RSSHubFeedEntry, RSSHubFeedSource
 from RSubscribeSummarizer.utils.logger import get_logger
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session, select
 
 
-logger = get_logger("FastAPI")
+logger = get_logger("FastAPI", log_file_path="./log/FastAPI.log")
 
 rss_urls: list[str] = ["https://rsshub.app/wallstreetcn/live/global/2"]
 
@@ -65,6 +66,9 @@ site.mount_app(app)
 @app.get("/fetch_all")
 @scheduler.scheduled_job("interval", name="Fetch All RSS URLs", seconds=3600)
 def fetch_all_rss():
+    """
+    Regular fetch from all given RSS and store into database
+    """
     # TODO: make this more elegant
     logger.info("Trigger fetch all")
     # TODO: Make interval a GET argument
@@ -73,6 +77,14 @@ def fetch_all_rss():
 
 # Start the scheduled task scheduler
 scheduler.start()
+
+
+@app.get("/feed_sources")
+def get_feed_sources() -> list[RSSHubFeedSource]:
+    logger.info("Get feed sources")
+    with Session(engine) as session:
+        sources = session.exec(select(RSSHubFeedSource)).all()
+    return sources
 
 
 if __name__ == "__main__":
