492bb445bead52f1cc4f1094a703aa382fcc6447
Da-Wei Lee <daviddwlee84@gmail.com>
2024-05-13 19:00:46 +0800

    Able to run SQL query on database

--- Streamlit/Overview.py
+++ Streamlit/Overview.py
@@ -1,4 +1,4 @@
-import sys
+# import sys
 import os
 import requests
 import urllib.parse
@@ -6,11 +6,12 @@ import streamlit as st
 import pandas as pd
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(curr_dir, ".."))
-from RSubscribeSummarizer.data.model import RSSHubFeedEntry, RSSHubFeedSource  # noqa
+# sys.path.append(os.path.join(curr_dir, ".."))
+# from RSubscribeSummarizer.data.model import RSSHubFeedEntry, RSSHubFeedSource  # noqa
 
 API_ROOT_PATH = "/api"
 API_REQUEST_ROOT = "http://api:8000"
+DATABASE_PATH = os.path.join(curr_dir, "..", "database.db")
 
 f"""
 # RSS Subscriber & Summarizer
@@ -41,6 +42,37 @@ def get_feed_sources() -> dict:
     ).json()
 
 
+import sqlite3
+import pandas as pd
+
+
+# TODO: maybe support all other database => use SQLModel/SQLAlchemy way..?
+@st.cache_data
+def get_feed_sources_with_entry_count() -> pd.DataFrame:
+    conn = sqlite3.connect(DATABASE_PATH)
+    query = """
+    SELECT 
+        RSSHubFeedEntry.rss_source, 
+        COUNT(*) AS count, 
+        RSSHubFeedSource.* 
+    FROM 
+        RSSHubFeedEntry 
+    INNER JOIN 
+        RSSHubFeedSource 
+    ON 
+        RSSHubFeedEntry.rss_source = RSSHubFeedSource.name 
+    GROUP BY 
+        RSSHubFeedEntry.rss_source;
+    """
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
+
+
 st.subheader("All RSS Feed Sources")
 feed_sources = get_feed_sources()
 st.dataframe(pd.DataFrame(feed_sources))
+
+
+st.subheader("All RSS Feed Sources with Counts")
+st.dataframe(get_feed_sources_with_entry_count())
