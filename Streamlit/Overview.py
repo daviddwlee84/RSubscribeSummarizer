6e88b8c617ab183cc8c6f840cd7a9103365089c8
Da-Wei Lee <daviddwlee84@gmail.com>
2024-05-13 14:49:51 +0800

    List feed sources with DataFrame

--- Streamlit/Overview.py
+++ Streamlit/Overview.py
@@ -3,6 +3,7 @@ import os
 import requests
 import urllib.parse
 import streamlit as st
+import pandas as pd
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(curr_dir, ".."))
@@ -17,6 +18,7 @@ f"""
 - [OpenAPI Documents]({urllib.parse.urljoin(API_ROOT_PATH, 'docs')})
 - [ReDoc Documents]({urllib.parse.urljoin(API_ROOT_PATH, 'redoc')})
 - [Admin (Scheduler)]({urllib.parse.urljoin(API_ROOT_PATH, 'admin')})
+- [RSSHub Document](https://docs.rsshub.app/guide/)
 """
 
 # https://stackoverflow.com/questions/61814887/how-to-convert-a-list-of-pydantic-basemodels-to-pandas-dataframe
@@ -30,11 +32,15 @@ def get_configure() -> dict[str, dict[str, str] | None]:
 st.subheader("Configured Server")
 st.json(get_configure())
 
+
 # https://stackoverflow.com/questions/63881885/fastapi-get-request-with-pydantic-list-field/70845425#70845425
-# @st.cache_data
-# def get_feed_sources() -> dict[str, dict[str, str] | None]:
-#     return requests.get(
-#         f"{urllib.parse.urljoin(API_REQUEST_ROOT, 'feed_sources')}"
-#     ).json()
-#
-# st.subheader("All RSS Feed Sources")
+@st.cache_data
+def get_feed_sources() -> dict:
+    return requests.get(
+        f"{urllib.parse.urljoin(API_REQUEST_ROOT, 'feed_sources')}"
+    ).json()
+
+
+st.subheader("All RSS Feed Sources")
+feed_sources = get_feed_sources()
+st.dataframe(pd.DataFrame(feed_sources))
